c42e2f22de40870d65a73b28196802642b6830e2
Dongwoon Hyun <dongwoon.hyun@stanford.edu>
2020-04-27 21:58:40 +0000

    TensorFlow DAS example

--- example_picmus_tf.py
+++ example_picmus_tf.py
@@ -1,10 +1,10 @@
-# File:       example_PICMUS.py
+# File:       example_picmus_tf.py
 # Author:     Dongwoon Hyun (dongwoon.hyun@stanford.edu)
-# Created on: 2020-03-12
-import torch
+# Created on: 2020-04-27
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
-from das_torch import DAS_PW
+from das_tf import DAS_PW
 from PlaneWaveData import PICMUSData
 from PixelGrid import make_pixel_grid
 
@@ -19,7 +19,7 @@ P = PICMUSData(database_path, acq, target, dtype)
 xlims = [P.ele_pos[0, 0], P.ele_pos[-1, 0]]
 zlims = [5e-3, 55e-3]
 wvln = P.c / P.fc
-dx = wvln / 4
+dx = wvln / 3
 dz = dx  # Use square pixels
 grid = make_pixel_grid(xlims, zlims, dx, dz)
 fnum = 1
@@ -29,22 +29,20 @@ dasN = DAS_PW(P, grid)
 idx = len(P.angles) // 2  # Choose center angle for 1-angle DAS
 das1 = DAS_PW(P, grid, idx)
 
-# Make the input data a torch tensor
-iqdata = (
-    torch.tensor(P.idata, dtype=torch.float, device=torch.device("cuda:0")),
-    torch.tensor(P.qdata, dtype=torch.float, device=torch.device("cuda:0")),
-)
+# Stack the I and Q data in the innermost dimension
+with tf.device("/gpu:0"):
+    iqdata = np.stack((P.idata, P.qdata), axis=-1)
 
 # Make 75-angle image
-idasN, qdasN = dasN.forward(iqdata)
-idasN, qdasN = idasN.detach().cpu().numpy(), qdasN.detach().cpu().numpy()
+idasN, qdasN = dasN(iqdata)
+idasN, qdasN = np.array(idasN), np.array(qdasN)
 iqN = idasN + 1j * qdasN  # Tranpose for display purposes
 bimgN = 20 * np.log10(np.abs(iqN))  # Log-compress
 bimgN -= np.amax(bimgN)  # Normalize by max value
 
 # Make 1-angle image
-idas1, qdas1 = das1.forward(iqdata)
-idas1, qdas1 = idas1.detach().cpu().numpy(), qdas1.detach().cpu().numpy()
+idas1, qdas1 = das1(iqdata)
+idas1, qdas1 = np.array(idas1), np.array(qdas1)
 iq1 = idas1 + 1j * qdas1  # Transpose for display purposes
 bimg1 = 20 * np.log10(np.abs(iq1))  # Log-compress
 bimg1 -= np.amax(bimg1)  # Normalize by max value
